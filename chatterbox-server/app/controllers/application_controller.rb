a19327bf50f6f7e933a8b6667f5670977672ceca
Stephen Crane <scrane88@gmail.com>
2021-09-03 08:50:16 -0400

    Add routes for Message CRUD

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add routes
-  
+  set :default_content_type, "application/json"
+
+  get "/messages" do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post "/messages" do
+    new_message = Message.create(username: params[:username], body: params[:body])
+    new_message.to_json
+  end
+
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(body: params[:body])
+    message.to_json
+  end
+
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
 end
